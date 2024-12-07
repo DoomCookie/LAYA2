c8ca8fc1eb02403d87b8a4622be3e22ae60e6120
Artem <xan232@gmail.com>
2024-12-07 12:18:52 +0300

    test2

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from test import foo
 #hello world
 
 def print_hi(name):
-    foo("42")
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    foo("13")
+    # Use breakpoint in the code line below to debug your script.
+    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
